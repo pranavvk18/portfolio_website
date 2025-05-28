7694e2e9dae7f0fc39d794fbfcc53198418047fe
kpranavv <pranavpk1807@gmail.com>
2025-05-28 20:57:58 +0530

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio_website", // 👈 Required for GitHub Pages deployment
+  // base: "/portfolio_website", // 👈 Required for GitHub Pages deployment
 
   server: {
     host: "::",
