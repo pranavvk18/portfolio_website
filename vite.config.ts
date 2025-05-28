95b68668fcff687ce15a12125d3a1536d43a5cae
kpranavv <pranavpk1807@gmail.com>
2025-05-28 21:06:48 +0530

    fourth commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: "/portfolio_website", // 👈 Required for GitHub Pages deployment
-
   server: {
     host: "::",
     port: 8080,
